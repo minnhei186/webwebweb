5f53d2980b1954463bcd1c3154f5f8e2fe81917e
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-08-26 17:06:48 +0900

    ローカルデータコントロール

--- index.php
+++ index.php
@@ -2,18 +2,35 @@
  date_default_timezone_set('Asia/Tokyo');
  session_start();
 
- if(!isset($_SESSION['response'])){
-     $_SESSION['response'] = [];
- }
+// if(!isset($_SESSION['response'])){
+//     $_SESSION['response'] = [];
+// }
+//
+// if(isset($_POST['msg'])){
+//     array_push($_SESSION['response'],$_POST['msg']);
+//     if(count($_SESSION['response'])>10){
+//             array_shift($_SESSION['response']);
+//     }
+// }
 
+ $file = __DIR__ . '/data/msg';
+ if(file_exists($file)){
+     $msg_list = unserialize(file_get_contents($file));
+ }else{
+     $msg_list = [];
+ }
+ 
  if(isset($_POST['msg'])){
-     array_push($_SESSION['response'],$_POST['msg']);
-     if(count($_SESSION['response'])>10){
-             array_shift($_SESSION['response']);
-     }
+     array_push($msg_list,$_POST['msg']);
  }
 
- $date = date("Y-m-d H:i");
+ if(!is_dir(dirname($file))){
+     mkdir(dirname($file),0700,true);
+ }
+ 
+ file_put_contents($file, serialize($msg_list)); 
+ 
+ $date = date("Y-m-d H:i"); 
 ?>
 
 <!DOCTYPE html>
@@ -33,9 +50,8 @@
  </form>
 
  <p>---------...</p>
-
  <?php 
-    foreach($_SESSION['response'] as $msg){
+    foreach($msg_list as $msg){
  ?>
      <p><?php echo $date . '______' . $msg; ?></p>
  <?php
